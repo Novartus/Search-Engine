a45462a85c0f15f542cccd53d384391f46c74949
Ghavan <54841979+Ghavan@users.noreply.github.com>
2022-04-08 08:57:36 +0530

    Update invertedIndex.java

--- src/invertedIndex/invertedIndex.java
+++ src/invertedIndex/invertedIndex.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,13 +18,19 @@ import java.util.Map.Entry;
 
 public class invertedIndex {
 
-  HashMap<Integer, String> wordMap;
+  // mapping of the filekeys (0 to n) and file names
+  HashMap<Integer, String> fileMap;
+  
+  // mapping of the (word and set of filekeys), checked to search word
   HashMap<String, HashSet<Integer>> trashMap;
   RankingWebPages rank;
   removeStopWords sameWord;
+  
+  // the format of println
+  private String sizeForm = "%-8s%-8s%-18s%-18s\n";
 
   public invertedIndex() {
-    wordMap = new HashMap<Integer, String>();
+    fileMap = new HashMap<Integer, String>();
     trashMap = new HashMap<String, HashSet<Integer>>();
     rank = new RankingWebPages();
   }
@@ -44,11 +51,13 @@ public class invertedIndex {
         String fileName = files[fileKey].getName().trim();
 
         // setting the keys and values having key and fileName
-        wordMap.put(fileKey, fileName);
+        fileMap.put(fileKey, fileName);
 
         String line;
         while ((line = file.readLine()) != null) {
           String[] wordArray = line.split("\\W+");
+//          System.out.println(line);
+//          System.out.println(Arrays.toString(wordArray));
 
           for (int j = 0; j < wordArray.length; j++) {
             if (!trashMap.containsKey(wordArray[j].toLowerCase())) {
@@ -56,16 +65,18 @@ public class invertedIndex {
             }
 
             trashMap.get(wordArray[j].toLowerCase()).add(fileKey);
-
+            
+            
+            // to check if the word already exists in the rank HashMap and if it not then put freq value to 1 
             if (!rank.freq.containsKey(wordArray[j].toLowerCase())) {
               HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
               freq.put(fileKey, 1);
 
               rank.freq.put(wordArray[j].toLowerCase(), freq);
-            } else {
-              HashMap<Integer, Integer> freq = rank.freq.get(
-                wordArray[j].toLowerCase()
-              );
+            } 
+//            if it exists then first check filekey exists in the set or not, if it is then increment by 1 
+            else {
+              HashMap<Integer, Integer> freq = rank.freq.get(wordArray[j].toLowerCase());
 
               if (!freq.containsKey(fileKey)) {
                 freq.put(fileKey, 1);
@@ -78,24 +89,19 @@ public class invertedIndex {
       }
     } catch (Exception e) {
       System.out.println(
-        "Exception in processCleanedTextFiles method: " +
-        files[fileKey].getName() +
-        " file not found because of " +
-        e
-      );
+        "Exception in processCleanedTextFiles method: " +files[fileKey].getName() +" file not found because of " +e);
     } finally {
       file.close();
     }
   }
 
-  // Fuction for searching text
+  // Function for searching text
   public List<String> searchText(String search) throws IOException {
     String[] words = search.split("\\W+");
-
+    
+    // if search word not exists then gives suggestion about search word
     if (!trashMap.containsKey(words[0].toLowerCase())) {
-      List<String> wordSug = removeStopWords.findSuggestion(
-        removeStopWords.stemWords(words[0].toLowerCase())
-      );
+      List<String> wordSug = removeStopWords.findSuggestion(removeStopWords.stemWords(words[0].toLowerCase()));
       for (String word : wordSug) {
         if (wordSug.size() > 1) {
           System.out.println("The few suggestion related to your search: ");
@@ -112,23 +118,30 @@ public class invertedIndex {
         );
         return null;
       }
-    } else {}
+    } else {
+    	System.out.println("Search results for the word \"" + search +"\"");
+    }
 
     int count = 1;
     List<String> files = new ArrayList<String>();
     List<Entry<Integer, Integer>> results = rank.sortingTexts(words);
+    
+    System.out.println("Total Search Results: " + results.size());
+    System.out.print("----------------------------------------------------------------- \n");
+    System.out.format(sizeForm , "Sr.No.", "File", "Occurrences", "File Title");
+    System.out.print("----------------------------------------------------------------- \n");
 
     HashSet<String> setFile = new HashSet<String>();
 
     for (Entry<Integer, Integer> entry : results) {
       if (count <= 10) {
-        String fileName = wordMap.get(entry.getKey());
+        String fileName = fileMap.get(entry.getKey());
         final List<String> lines = Files.readAllLines(
           Paths.get("Converted Text Files/" + fileName),
           StandardCharsets.ISO_8859_1
         );
         if (!setFile.contains(lines.get(1))) {
-          // System.out.format(sizeForm, count, fileName, entry.getValue(), lines.get(1));
+           System.out.format(sizeForm, count, fileName, entry.getValue(), lines.get(1));
           files.add(lines.get(0));
           files.add(lines.get(1));
           setFile.add(lines.get(1));
@@ -154,5 +167,13 @@ public class invertedIndex {
     BufferedReader input = new BufferedReader(isr);
     String text = input.readLine();
     ivr.searchText(text);
+  }
+  
+  public static void main(String[] args) {
+	  try {
+		  executePageRanking();
+	  }catch (Exception e) {
+		System.out.println("Exception in main method in invertedIndex class: "+e);
+	}
   }
 }
